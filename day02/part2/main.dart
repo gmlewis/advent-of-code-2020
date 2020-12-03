36c155fb824d2a8545d7b2c4af20f951f8da8201
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-02 22:51:07 -0500

    Add Dart solution

---
+++ day02/part2/main.dart
@@ -0,0 +1,41 @@
+// -*- compile-command: "dart main.dart ../input.txt"; -*-
+
+import 'dart:io';
+
+main(List<String> args) {
+  for (var arg in args) {
+    process(arg);
+  }
+
+  print('Done.');
+}
+
+process(String filename) {
+  print('Processing $filename ...');
+
+	var lineRE  = RegExp(r'^(\d+)-(\d+) ([a-z]): (.*)$');
+
+  var count = 0;
+  var lines = File(filename).readAsLinesSync();
+  for (var line in lines) {
+    var m = lineRE.allMatches(line).first;
+
+    var start = int.parse(m.group(1));
+    var end = int.parse(m.group(2));
+    var letter = m.group(3);
+    var passwd = m.group(4);
+
+    if (valid(start, end, letter, passwd)) {
+      count++;
+    }
+  }
+
+  print('$count valid passwords');
+}
+
+bool valid(int start, int end, String letter, String passwd) {
+  // print('$start-$end $letter: $passwd');
+  var first = passwd.substring(start-1,start) == letter;
+  var second = passwd.substring(end-1,end) == letter;
+  return first != second;
+}
