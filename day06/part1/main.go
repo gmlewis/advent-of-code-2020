f26c3b6efd0b3799b7d92ef118ce2b2181c69890
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-06 09:09:54 -0500

    Add day06 Go solutions

---
+++ day06/part1/main.go
@@ -0,0 +1,92 @@
+// -*- compile-command: "go run main.go ../input.txt"; -*-
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+var (
+	verbose = flag.Bool("v", false, "Verbose log messages")
+)
+
+func main() {
+	flag.Parse()
+
+	for _, arg := range flag.Args() {
+		process(arg)
+	}
+
+	log.Printf("Done.")
+}
+
+func process(filename string) {
+	groups := readGroups(filename)
+	// fmt.Printf("%v\n", strings.Join(groups, "\n"))
+	var count int
+	for _, group := range groups {
+		group = strings.ReplaceAll(group, "\n", "")
+		// fmt.Printf("%v\n", group)
+		runes := map[rune]int{}
+		for _, r := range group {
+			runes[r]++
+		}
+		// fmt.Printf("%v\n", len(runes))
+		count += len(runes)
+	}
+	fmt.Printf("Solution: %v\n", count)
+}
+
+func readGroups(filename string) []string {
+	log.Printf("Processing %v ...", filename)
+	buf, err := ioutil.ReadFile(filename)
+	check("ReadFile: %v", err)
+	groups := strings.Split(string(buf), "\n\n")
+	return groups
+}
+
+func processSpaces(lines []string) []int {
+	var result []int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		v := spaceID(line)
+		result = append(result, v)
+	}
+
+	return result
+}
+
+func spaceID(s string) int {
+	rowS := s[0:7]
+	colS := s[7:10]
+	rowS = strings.ReplaceAll(rowS, "F", "0")
+	rowS = strings.ReplaceAll(rowS, "B", "1")
+	colS = strings.ReplaceAll(colS, "L", "0")
+	colS = strings.ReplaceAll(colS, "R", "1")
+
+	row, err := strconv.ParseInt(rowS, 2, 64)
+	check("row ParseInt: %v", err)
+	col, err := strconv.ParseInt(colS, 2, 64)
+	check("col ParseInt: %v", err)
+
+	return int(row*8 + col)
+}
+
+func check(fmtStr string, args ...interface{}) {
+	if err := args[len(args)-1]; err != nil {
+		log.Fatalf(fmtStr, args...)
+	}
+}
+
+func logf(fmt string, args ...interface{}) {
+	if *verbose {
+		log.Printf(fmt, args...)
+	}
+}
