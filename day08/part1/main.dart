6d64c206ff24ade6fc5e27e7e07d29d73c4e666e
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-08 08:11:26 -0500

    Add day08 part1 Dart solution

--- day08/part1/main.dart
+++ day08/part1/main.dart
@@ -1,4 +1,4 @@
-// -*- compile-command: "dart main.dart ../example1.txt ../example2.txt ../input.txt"; -*-
+// -*- compile-command: "dart main.dart ../example1.txt ../input.txt"; -*-
 
 import 'dart:io';
 
@@ -10,66 +10,62 @@ main(List<String> args) {
   print('Done.');
 }
 
-class Contains {
-  int quant;
-  String color;
-  Contains({this.quant, this.color});
+class Instruction {
+  String op;
+  int arg;
+  Instruction({this.op, this.arg});
 
-  toString() => 'Contains(quant: $quant, color: $color)';
+  toString() => 'op: $op, arg: $arg';
 }
 
 process(String filename) {
   print('Processing $filename ...');
 
-  var lineRE = RegExp(r'^(.*?) bags contain (.*)\.$');
-  var containsRE = RegExp(r',?\s*(\d+) (.*?) bags?');
-
-  var rules = Map<String,List<Contains>>();
-
+  var program = List<Instruction>();
   var lines = File(filename).readAsLinesSync();
   for (var line in lines) {
-    var m = lineRE.firstMatch(line);
-    // print('${m.group(0)}; ${m.group(1)}; ${m.group(2)}');
-    if (m.group(2) == 'no other bags') {
-      rules[m.group(1)] = [];  // empty list
-      // print('No bags: rules[${m.group(1)}] = []');
-      continue;
-    }
-
-    var m2 = containsRE.allMatches(m.group(2));
-    m2.forEach((f) {
-        var q = int.parse(f.group(1));
-        // print('${f.group(0)}; ${f.group(1)}; ${f.group(2)}');
-        var c = Contains(quant: q, color: f.group(2));
-        // if (rules.containsKey(m.group(1))) {
-        //   print('BEFORE: rules[${m.group(1)}] = ${rules[m.group(1)]}');
-        // } else {
-        //   print('BEFORE: rules[${m.group(1)}] = null');
-        // }
-        rules.update(m.group(1), (cs) {
-            // print('CS BEFORE: cs=$cs');
-            cs.add(c);
-            // print('CS AFTER: cs=$cs');
-            return cs;  // CRITICAL TO PROVIDE!!!
-          }, ifAbsent: () => [c]);
-        // print('AFTER: rules[${m.group(1)}] = ${rules[m.group(1)]}');
-    });
+    var parts = line.split(' ');
+    var ins = Instruction(op: parts[0], arg: int.parse(parts[1]));
+    program.add(ins);
   }
 
-  var count = contains("shiny gold", rules);
-
-  print('Solution: ${count-1}');
+  var cpu = CPU(program: program);
+  cpu.execute();
 }
 
-int contains(String key, Map<String,List<Contains>> rules) {
-  var total = 1;
-
-  if (!rules.containsKey(key) || rules[key] == null) return 1;
-
-  rules[key].forEach((v) {
-      var count = contains(v.color, rules);
-      total += (count * v.quant);
+class CPU {
+  int accumulator;
+  int ip;
+  List<Instruction> program;
+  CPU({
+      this.accumulator = 0,
+      this.ip = 0,
+      this.program,
   });
 
-  return total;
+  execute() {
+    var seen = Set<int>();
+
+    while (true) {
+      if (seen.contains(ip)) {
+        print('Solution Accumulator = $accumulator');
+        return;
+      }
+      seen.add(ip);
+
+      var ins = program[ip];
+      switch (ins.op) {
+        case 'nop':
+        ip++;
+        break;
+        case 'acc':
+        accumulator += ins.arg;
+        ip++;
+        break;
+        case 'jmp':
+        ip += ins.arg;
+        break;
+      }
+    }
+  }
 }
