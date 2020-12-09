f823568177353e0b8ba2a192ea9c36837effdfc8
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-09 17:22:22 -0500

    Add day01 part1 V solution

---
+++ day01/part1/main.v
@@ -0,0 +1,24 @@
+// -*- compile-command: "v run main.v ../input.txt"; -*-
+import os
+import strconv
+
+fn main() {
+	for arg in os.args[1..] {
+		process(arg)
+	}
+	println('Done.')
+}
+
+fn process(filename string) {
+	println('Processing $filename ...')
+	lines := os.read_lines(filename) or { panic(err) }
+	mut vals := map[string]bool{}
+	for line in lines {
+		v := strconv.atoi(line)
+		if vals['$v'] {
+			println('$v + ${2020 - v} = 2020\n$v * ${2020 - v} = ${v * (2020 - v)}')
+			break
+		}
+		vals['${2020 - v}'] = true
+	}
+}
