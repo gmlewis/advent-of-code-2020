31cd17776af6bbb611ddb399d7001f2a3739852c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-17 23:13:57 -0500

    Finished day13

--- day13/part2/main.go
+++ day13/part2/main.go
@@ -29,21 +29,19 @@ func main() {
 func process(filename string) {
 	lines := readLines(filename)
 	busIDs := strings.Split(lines[1], ",")
-	ids := map[uint64]uint64{}
-	sortedIDs := []uint64{}
-	// multipliers := map[uint64]uint64{}
+	ids := map[int64]int64{}
+	sortedIDs := []int64{}
 
-	var max uint64
-	var start uint64 = 1
+	var max int64
+	var start int64 = 1
 	for i, v := range busIDs {
 		if v == "x" {
 			continue
 		}
-		id, err := strconv.ParseUint(v, 10, 64)
+		id, err := strconv.ParseInt(v, 10, 64)
 		check("id: %v", err)
-		ids[id] = uint64(i)
+		ids[id] = int64(i)
 		sortedIDs = append(sortedIDs, id)
-		// multipliers[id] = 1
 		if id > max {
 			max = id
 		}
@@ -54,94 +52,38 @@ func process(filename string) {
 	log.Printf("ids=%#v", ids)
 	log.Printf("sortedIDs=%#v", sortedIDs)
 
-	// for k := range ids {
-	// 	for k2 := range multipliers {
-	// 		if k != k2 {
-	// 			multipliers[k2] *= k
-	// 		}
-	// 	}
-	// }
-	// log.Printf("multipliers=%#v", multipliers)
-
-	// solve(max, start, ids, sortedIDs)
-
 	offset := ids[max]
 	log.Printf("offset=%#v", offset)
 
-	timestamp := start/2 - (start / 2 % max)
+	timestamp := int64(100000000000000) - int64(100000000000000)%max
+	log.Printf("starting at timestamp %v", timestamp)
 
-	for ; timestamp > start/3; timestamp -= max {
-		if isSolution(timestamp, offset, ids) {
+	for {
+		skip, ok := isSolution(timestamp, offset, ids)
+		if ok {
 			log.Printf("Solution: %v", timestamp-offset)
 			return
 		}
+		timestamp += skip
 	}
 	log.Fatalf("no solution found")
 }
 
-func solve(max, start uint64, ids map[uint64]uint64, sortedIDs []uint64) {
-	log.Printf("solve(max=%v, start=%v, ids=%v, sortedIDs=%v", max, start, ids, sortedIDs)
-	// var ts uint64
-	// var delta uint64
-	ch := make(chan uint64, *workers)
-	for i, id := range sortedIDs {
-		factors := []uint64{}
-		for j := uint64(1); j < 10; j++ {
-			factors = append(factors, j*id-ids[id])
-		}
-		log.Printf("id=%v, solution must be at N[%v]*%v-%v %+v",
-			id, i, id, ids[id], factors)
-
-		if i == 0 {
-			go func(out chan<- uint64, k, d uint64) {
-				v := k - d
-				for {
-					// log.Printf("f(%v): emiting %v", k, v)
-					out <- v
-					v += k
-				}
-			}(ch, id, ids[id])
-		} else {
-			out := make(chan uint64, *workers)
-			go func(ch <-chan uint64, out chan<- uint64, k, d uint64) {
-				v := k - d
-				for next := range ch {
-					// log.Printf("f(%v): got %v", k, next)
-					for v <= next {
-						v += k
-					}
-					if v == next {
-						// log.Printf("f(%v): emiting %v", k, v)
-						out <- v
-					}
-				}
-			}(ch, out, id, ids[id])
-			ch = out
-		}
-	}
-
-	v := <-ch
-	log.Fatalf("Solution: %v", v)
-
-	// t := uint64(1068781)
-	// prod := (t + 4) * (t + 6) * (t + 7) * (t + 1) * t
-	// log.Printf("(t+4)(t+6)(t+7)(t+1)(t)=%v %% %v = %v", prod, start, prod%start)
-	// log.Printf("(t+4)(t+6)(t+7)(t+1)(t)=%v / %v = %v", prod, start, prod/start)
-}
-
-func lcm(a, b uint64) uint64 {
+func lcm(a, b int64) int64 {
 	return a * b / gcd(a, b)
 }
 
-func gcd(a, b uint64) uint64 {
+func gcd(a, b int64) int64 {
 	for b > 0 {
 		a, b = b, a%b
 	}
 	return a
 }
 
-func isSolution(timestamp, offset uint64, ids map[uint64]uint64) bool {
+func isSolution(timestamp, offset int64, ids map[int64]int64) (int64, bool) {
 	// log.Printf("timestamp=%v", timestamp)
+	skip := int64(1)
+	solved := true
 	for k, v := range ids {
 		if timestamp == 1068785 {
 			log.Printf("(ts=%v - off=%v + v=%v = %v)%%(k=%v) = %v; (%v/%v)=%v",
@@ -149,10 +91,12 @@ func isSolution(timestamp, offset uint64, ids map[uint64]uint64) bool {
 				(timestamp - offset + v), k, (timestamp-offset+v)/k)
 		}
 		if (timestamp-offset+v)%k != 0 {
-			return false
+			solved = false
+			continue
 		}
+		skip *= k
 	}
-	return true
+	return skip, solved
 }
 
 func readLines(filename string) []string {
